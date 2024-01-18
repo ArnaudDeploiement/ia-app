ce451b7d00e7fb6da86495f55b28bbc24e211654
ArnaudDeploiement <127393720+ArnaudDeploiement@users.noreply.github.com>
2024-01-18 15:56:17 +0100

    V.1

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../../components/hero/hero.component';
+import { FeaturesComponent } from '../../components/features/features.component';
+import { ContentComponent } from '../../components/content/content.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent,FeaturesComponent, ContentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
